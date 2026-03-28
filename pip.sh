b65a81f7227f0481cacbef650606fcd6dc76fde4
Void <jnxkcxkjjcjkccm@gmail.com>
2026-03-28 14:38:41 -0400

    Create pip.sh

---
+++ pip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# PyPhone - pip.sh
+
+echo "Setting up PyPhone environment..."
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Install required packages (add more if needed)
+pip install colorama
+
+echo "All dependencies installed."
